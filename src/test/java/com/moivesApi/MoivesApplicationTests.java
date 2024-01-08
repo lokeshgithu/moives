c4672777d2975c777673f2343798d2539fde61a8
@lokeshgithu <lokeshgithub786@gmail.com>
2024-01-08 16:45:58 +0530

    moives api in development

---
+++ src/test/java/com/moivesApi/MoivesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moivesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoivesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
